967d26a60fbdd05f5af5bfa46ec4ca312602760b
FW <tweakimp@gmail.com>
2018-02-14 23:28:53 +0100

    Delete testdata_validation.py

--- testdata_validation.py
+++
@@ -1,9 +0,0 @@
-import main
-import testdata
-
-for test in testdata.tests:
-    testboard = main.TFBoard()
-    testboard.matrix = test[0]
-    testboard.makeMove(test[1])
-    testboard.checkWin()
-    print(test[2], testboard.endGame)
